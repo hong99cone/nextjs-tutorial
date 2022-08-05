9e6c7c5e19f82994a468b01f13d1246faf49013b
hong <03mm007mm@gmail.com>
2022-08-05 17:59:07 +0900

    next.js ~ing

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/global.css'
+// This App component is the top-level component
+
+export default function App ({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
